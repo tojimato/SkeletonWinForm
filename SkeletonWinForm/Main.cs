8b07f75c8cfbff0d05e2dd3e7f146d999586bc80
tojimato <tojimato@gmail.com>
2015-12-07 09:02:47 +0200

    Commit To master
    ASdasd

---
+++ SkeletonWinForm/Main.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SkeletonWinForm
+{
+    /*
+     Basirisiz insert sonrasi delete islemi yapilamiyor.
+     * 
+     */
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            var appContext = new AppData.SkeletonLINQDataContext();
+
+            var x = new TSQL.TSQL<AppData.SkeletonLINQDataContext>();
+
+            
+            //Select Data Source From Linq To SQL
+            gvTestGrid.DataSource = x.fetch<AppData.MUH_FATURA>()
+                .Select(y => new { y.MUH_FATURAID })
+                .Where((refx => refx.MUH_FATURAID == 59 || refx.MUH_FATURAID == 58 || refx.MUH_FATURAID == 77));
+
+            // /* ##################################### Run Custom Query.  ################################# */  
+            var result = x.runQuery<AppData.MUH_FATURA>(@"Select * FROM PRJTHOR.dbo.MUH_FATURA WHERE MUH_FATURAID={0}",58);
+            Console.WriteLine("Run Query : "+result.Count().ToString());
+
+           
+            /* ##################################### Insert ################################# */ 
+            AppData.IL Il = new AppData.IL(){
+                IL_ADI = "Torzincan",
+                IL_ADI_BUYUK = "TORZINCAN",
+                IL_ADI_KUCUK = "torzincan",
+                IL_KODU = "99",
+                PLAKA = 101,
+                IL_ID = 1011
+            };
+             var insert_result = x.insert<AppData.IL>(Il);
+           
+            //Sorgu suresi, Hata Mesaji vs vs..
+
+            /* ####################################### Update ##############################################*/
+
+            AppData.IL IlUpdate = new AppData.IL()
+            {
+                IL_ADI = "Torzinc33an",
+                IL_ADI_BUYUK = "TORZINCAN333333",
+                IL_ADI_KUCUK = "torzin33can",
+                IL_KODU = "99",
+                PLAKA = 101,
+                IL_ID = 1011
+            };
+
+            var update_result = x.update<AppData.IL>(IlUpdate, (il => il.IL_ID == IlUpdate.IL_ID));
+            /* ###################################### Delete ##############################################*/
+
+            var delete_result = x.delete<AppData.IL>(d=> d.IL_ID == 70);
+           
+           
+        }
+    }
+}
---
+++ TSQL/TSQL.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace TSQL
+{
+    public class TSQL<Context>
+        where Context : System.Data.Linq.DataContext, new()
+    {
+        Context obj;
+
+        public TSQL()
+        {
+            this.obj = new Context();
+            if (this.obj.Connection.State == ConnectionState.Open)
+                this.obj.Connection.Close();
+        }
+
+        //Generic get table class
+        public IQueryable<Class> fetch<Class>() where Class : class
+        {
+            try
+            {
+                return (from i in this.obj.GetTable<Class>() select i);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        //Custom Run Query 
+        public IList<Class> runQuery<Class>(string query, params object[] parameters) where Class : class
+        {
+            try
+            {
+                IList<Class> result;
+                result = obj.ExecuteQuery<Class>(query, parameters).ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {               
+                throw new Exception(ex.Message);
+            }
+
+        }
+        //Insert Class
+        public bool insert<Class>(Class cls) where Class : class
+        {
+            var result = false;
+
+            try
+            {
+                this.obj.GetTable<Class>().InsertOnSubmit(cls);
+                this.obj.SubmitChanges();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                
+            }
+
+            return result;
+        }
+        //Update Class
+        public bool update<Class>(Class cls, Expression<Func<Class, bool>> expression) where Class : class
+        {
+
+            var result = false;
+            try
+            {
+                var rmv = this.delete(expression);
+                if (rmv)
+                {
+                    this.insert<Class>(cls);
+                }
+                result = true;
+            }
+            catch (Exception ex)
+            {
+              
+                result = false;
+               
+            }
+            return result;
+        }
+        //Delete Class
+        public bool delete<Class>(Expression<Func<Class, bool>> expression) where Class : class
+        {
+            var result = false;
+            try
+            {
+                var objects = this.obj.GetTable<Class>().Where(expression);
+                var deletionObj = this.obj.GetTable<Class>();
+                foreach(var item in objects)
+                {
+                   deletionObj.DeleteOnSubmit(item);
+                }
+                this.obj.SubmitChanges();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+
+    }
+}
